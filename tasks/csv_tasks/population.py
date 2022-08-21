8a09c0ae05a9b946f3557f0ae021337dd0df0651
Kirillchumnoy <rebilion@gmail.com>
2022-08-21 19:45:09 +0300

    done_population.py

--- tasks/csv_tasks/population.py
+++ tasks/csv_tasks/population.py
@@ -3,3 +3,16 @@
 файла world_population.csv и функцию должна возвращать год, в котором
 наибольший процент прироста населения.
 """
+import csv
+
+
+def max_delta(file_name: str):
+    perc = 0
+    year = {}
+    with open(file_name) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        for row in reader:
+            if float(row['ChangePerc']) > perc:
+                perc = float(row['ChangePerc'])
+                year[perc] = row['Year']
+        return year[perc]
